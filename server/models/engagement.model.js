20b6dc53dc4eaa2a85bac276b3848299e9488174
nainascode <nainadugar.official@gmail.com>
2026-04-24 16:35:48 +0530

    feat: engagement model done

---
+++ server/models/engagement.model.js
@@ -0,0 +1,37 @@
+import mongoose, { Schema } from "mongoose";
+const engagementSchema= new mongoose.Schema({
+    user:{
+        type: Schema.Types.ObjectId,
+        ref: "User",
+        required: true,
+        index: true
+    },
+    post:{
+        type: Schema.Types.ObjectId,
+        ref: "Post",
+        required: true,
+        index: true
+    },
+    type:{
+        type: String,
+        enum: [like, save, view],
+        required: true,
+        default: view
+    }
+},
+{ timestamps : true })
+
+/*  
+    Compound index:
+    Ensures ONE like/save per user per post
+*/
+engagementSchema.index({
+    user:1, type:1, post:1
+},{
+    unique:true,
+    partialFilterExpression:{
+        type:{$in: ["like", "save"]}
+    }
+})
+const Engagement = mongoose.model("Engagement", engagementSchema);
+export default Engagement;
