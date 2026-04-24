d9efadb72c63c8507bbd08baf102b6eda6cc2f54
nainascode <nainadugar.official@gmail.com>
2026-04-24 13:42:40 +0530

    feat: view all posts functionality

--- server/controllers/post.controller.js
+++ server/controllers/post.controller.js
@@ -4,6 +4,7 @@ import { ApiResponse } from '../utils/ApiResponse.js';
 import ImageKitService from "../services/imagekit.service.js";
 import Post from '../models/post.model.js';
 import mongoose from "mongoose";
+import User from '../models/user.model.js';
 
 //create post
 const createPost = asyncHandler(
@@ -46,10 +47,11 @@ const createPost = asyncHandler(
 const viewPostById = asyncHandler(
     async(req,res)=>{
         //in post.route.js file, the parameter name was different.
-        const { id } = req.params;
+        const { id } = req.params; //post id
         console.log(id);
 
         const post = await Post.findById(id).populate("author", "username avatar");
+        console.log(post);
         
         if(!post){
             throw ApiError(404, "No post found");
@@ -58,9 +60,28 @@ const viewPostById = asyncHandler(
             .json(new ApiResponse(200, post, "Single post"));
     }
 )
-const viewAllPost = asyncHandler(
+const viewAllPosts = asyncHandler(
+    //first get the username of the user whose post you want to see and then get its id
+    //now, use Post to find all the posts by the author with that id
+
     async (req,res) => {
+        //user id
+        const { username } = req.params;
+        console.log(username);
+
+        //find returns as array not object
+        // const currUser = await User.find({username})
+
+        //findOne returns an object
+        const currUser = await User.findOne({username})
         
+        const posts = await Post.find({ author: currUser._id }).sort({createdAt:-1})
+        
+        if(posts.length===0){
+            throw new ApiError(404, "No post found");
+        }
+        return res.status(200)
+            .json(new ApiResponse(200, posts, `Posts by ${username}`));
     }
 )
 
@@ -78,4 +99,4 @@ const deletePost = asyncHandler(
     }
 )
 
-export { createPost, viewPostById, editPost, deletePost }
+export { createPost, viewPostById, viewAllPosts, editPost, deletePost }
--- server/routes/post.route.js
+++ server/routes/post.route.js
@@ -1,7 +1,7 @@
 import Router from "express"
 import upload from "../middlewares/upload.middleware.js";
-import { verifyUser } from "../middlewares/auth.middleware";
-import { createPost, viewPostById, editPost, deletePost } from "../controllers/post.controller";
+import { verifyUser } from "../middlewares/auth.middleware.js";
+import { createPost, viewPostById, viewAllPosts, editPost, deletePost } from "../controllers/post.controller.js";
 const router = Router();
 
 router.use(verifyUser);
@@ -9,5 +9,6 @@ router.use(verifyUser);
 router.route("/create-post").post(upload.array("posts", 5), createPost);//working
 
 //get
-router.route("/view-post/:id").get(viewPostById);
+router.route("/single-post/:id").get(viewPostById);
+router.route("/all-posts/:username").get(viewAllPosts);
 export default router;
