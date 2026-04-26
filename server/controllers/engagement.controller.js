31762c3a85fe588bacc3f4e2526947297378a056
nainascode <nainadugar.official@gmail.com>
2026-04-26 22:54:42 +0530

    fix: asyncHandler catch block, removed asyncHandler from services and import statement in controller

--- server/controllers/engagement.controller.js
+++ server/controllers/engagement.controller.js
@@ -1,6 +1,7 @@
 import asyncHandler from "../utils/asyncHandler.js";
 import { toggleEngagement, viewCount } from '../services/engagement.service.js'
 import Post from "../models/post.model.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 import { ApiError } from "../utils/ApiError.js";
 
 const toggleLike = asyncHandler(
@@ -9,15 +10,17 @@ const toggleLike = asyncHandler(
         const {id}= req.params;
          
         const post = await Post.findById(id);
+        
         if(!post){
             throw new ApiError(404, "Post not found")
         }
-        const result = await toggleEngagement({
-            userId: req.user._id,
-            postId: id,
-            type: "like"
-        });
-
+        const result = await toggleEngagement(
+            req.user._id,
+            id,
+            "like"
+        );
+        console.log(result);
+        
         return res.json(
             new ApiResponse(200, "", `Like ${result.action}`)
         );
@@ -32,11 +35,11 @@ const toggleSave = asyncHandler(
         if(!post){
             throw new ApiError(404, "Post not found")
         }
-        const result = await toggleEngagement({
-            userId: req.user._id,
-            postId: id,
-            type: "save"
-        });
+        const result = await toggleEngagement(
+            req.user._id,
+            id,
+            "save"
+        );
 
         return res.json(
             new ApiResponse(200, "", `Save ${result.action}`)
@@ -52,12 +55,8 @@ const addPostView = asyncHandler(
         if(!post){
             throw new ApiError(404, "Post not found")
         }
-        const result = await viewCount({
-            userId: req.user._id,
-            postId: id,
-            type: "view"
-        });
-
+        const result = await viewCount(req.user._id, id, "view")
+        
         return res.json(
             new ApiResponse(200, "", `View ${result.action}`)
         );
--- server/services/engagement.service.js
+++ server/services/engagement.service.js
@@ -4,108 +4,89 @@ import Post from "../models/post.model.js";
 import Engagement from "../models/engagement.model.js";
 import { ApiError } from "../utils/ApiError.js";
 //ensures only one like and one save per user is allowed
-const toggleEngagement = asyncHandler(
-    async (userId, postId, type) => {
-        //session object to ensure consistency amongst the whole process
-        const session = await mongoose.startSession();
+const toggleEngagement = 
+  async (userId, postId, type) => {
+    const session = await mongoose.startSession();
+    //we are going to first find if such document already exists or not in the db using the data
+    //according we will toggle the info
+    try {
+        session.startTransaction();
+        const exists = await Engagement.findOne({
+            user: userId,
+            post: postId,
+            type
+        }).session(session);
 
-        try {
-            
-            //we are going to first find if such document already exists or not in the db using the data
-            //according we will toggle the info
-
-            session.startTransaction();
-
-            const exists = await Engagement.findOne(
-                {
-                    user: userId,
-                    post : postId,
-                    type
-                }
-            ).session(session)
-
-            if(exists){
-                await Engagement.deleteOne(
-                    { _id: existing._id },
-                    { session }
-                )
-                await Post.updateOne(
-                    {
-                        _id: postId
-                    },
-                    {
-                        $inc: {[`${type}sCount`]:-1}
-                    },
-                    { session }
-                )
-                await session.commitTransaction()
-                session.endSession()
-                return{ action: removed }
-
-            }
-            await Engagement.create(
-                [
-                    {
-                        user: userId,
-                        post: postId,
-                        type
-                    }
-                ],
+        if (exists) {
+            await Engagement.deleteOne(
+                { _id: exists._id },
                 { session }
-            )
+            );
+
             await Post.updateOne(
-                {
-                    _id: postId
-                },
-                {
-                    $inc: {[`${type}sCount`]:1}
-                },
+                { _id: postId },
+                { $inc: { [`${type}sCount`]: -1 } },
                 { session }
-            )
+            );
             await session.commitTransaction();
-            session.endSession();
-            return { action : updated }
-
-        } catch (error) {
-            await session.abortTransaction()
-            session.endSession()
-            throw error
+            return { action: "removed" };
         }
+
+        await Engagement.create(
+            [{
+                user: userId,
+                post: postId,
+                type
+            }],
+            { session }
+        );
+        
+        await Post.updateOne(
+            { _id: postId },
+            { $inc: { [`${type}sCount`]: 1 } },
+            { session }
+        );
+        await session.commitTransaction();
+        return { action: "added" };
+    } 
+    catch (error) {
+        await session.abortTransaction();
+        throw error;
+    } 
+    finally {
+        session.endSession();
     }
-)
+}
 //makes sure that per user the view count happens after 2mins once a post has been viewed
-const viewCount = asyncHandler(
+const viewCount = 
     async (userId, postId, type) => {
-            //we are going to check if it has been 5mins or not since the post was last viewed by that user
-            const recentView = await Engagement.findOne(
-                {
-                    user: userId,
-                    post: postId,
-                    type:"view",
-                    createdAt: { $gt: new Date(Date.now() - 2 * 60 * 1000) } //2 mins
-                })
-
-            if(recentView){
-                return { action: ignored }
-            }
-            await Engagement.create(
-                {
-                     user: userId,
-                    post: postId,
-                    type:"view"
-
-                },
-            )
-            await Post.updateOne(
-                { _id : post._id},
-                {
-                    $inc: {
-                        viewsCount : 1
-                    }
+        //we are going to check if it has been 5mins or not since the post was last viewed by that user
+        const recentView = await Engagement.findOne(
+            {
+                user: userId,
+                post: postId,
+                type:"view",
+                createdAt: { $gt: new Date(Date.now() - 2 * 60 * 1000) } //2 mins
+            })
+        if(recentView){
+            return { action: "ignored" }
+        }
+        await Engagement.create(
+            {
+                user: userId,
+                post: postId,
+                type:"view"
+            },
+        )
+        await Post.updateOne(
+            { _id : postId},
+            {
+                $inc: {
+                    viewsCount : 1
                 }
-            )
-            return { action: updated }
+            }
+        )
+        return { action: "updated" }
     }
-)
 
 export { toggleEngagement, viewCount }
